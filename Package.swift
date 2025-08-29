cec370159dc8fc0905577677312367791ea39094
Klarna Mobile SDK <67329369+klarna-msdk@users.noreply.github.com>
2025-08-29 13:23:14 +0000

    Release Klarna Mobile SDK iOS v2.7.3

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KlarnaCore",
-            url: "https://x.klarnacdn.net/mobile-sdk/ios/frameworks/KlarnaCore/2.7.2/KlarnaCore.xcframework.zip",
-            checksum: "b734668ec96178dc9867a601a5d7eef4ef8ac0af9b39f9813cae348e332ddcbb"
+            url: "https://x.klarnacdn.net/mobile-sdk/ios/frameworks/KlarnaCore/2.7.3/KlarnaCore.xcframework.zip",
+            checksum: "de17727a6fcc1477c47b5e9c1a76c2314ab58e8244547a74b45567fc2e9ee441"
         ),
         .binaryTarget(
             name: "KlarnaMobileSDK",
-            url: "https://x.klarnacdn.net/mobile-sdk/ios/frameworks/KlarnaMobileSDK/2.7.2/KlarnaMobileSDK.xcframework.zip",
-            checksum: "3b7fc9aed9103f0170e494a08cba8e6c94c16951563ba84ceea9df14e600d627"
+            url: "https://x.klarnacdn.net/mobile-sdk/ios/frameworks/KlarnaMobileSDK/2.7.3/KlarnaMobileSDK.xcframework.zip",
+            checksum: "a86c4ccff5cbefacf5b3d75be4d10c2b725b2dbb187a8c10895bf897df1adafb"
         )
     ]
 )
